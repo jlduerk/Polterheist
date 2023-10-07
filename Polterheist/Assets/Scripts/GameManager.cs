2c8c974fae34053e376b77e41957eef15658de6b
DylValevv <dylan.valev@gmail.com>
2023-10-07 14:27:40 -0400

    team data listener

--- Polterheist/Assets/Scripts/GameManager.cs
+++ Polterheist/Assets/Scripts/GameManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour {
     private static GameManager instance;
     public static TeamData[] teamDatas;
 
+    private PlayerInputManager playerInputManager;
+
+    #region Monobehavior
     private void Awake() {
         // check if the instance already exists and destroy the new instance if it does
         if (instance != null && instance != this) {
@@ -16,7 +21,17 @@ public class GameManager : MonoBehaviour {
         instance = this;
         // prevent the instance from being destroyed when loading new scenes
         DontDestroyOnLoad(this.gameObject);
+        playerInputManager = FindObjectOfType<PlayerInputManager>();
     }
+
+    private void OnEnable() {
+        playerInputManager.onPlayerJoined += OnPlayerJoined;
+    }
+
+    private void OnDisable() {
+        playerInputManager.onPlayerJoined -= OnPlayerJoined;
+    }
+    #endregion
 
     public static TeamData GetTeamData(TeamData.Team teamToRequest) {
         foreach (TeamData teamData in teamDatas) {
@@ -26,5 +41,9 @@ public class GameManager : MonoBehaviour {
         }
         Debug.LogError($"{teamToRequest} is not in GameManager TeamData array!");
         return null;
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput) {
+        Debug.Log("Player joined with device: " + playerInput.gameObject.name);
     }
 }
