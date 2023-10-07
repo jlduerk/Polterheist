b052a8969a5393cee6602677f0332ab493da6571
kslokhandwala2022 <kslokhandwala@wpi.edu>
2023-10-07 15:28:58 -0400

    env scene setup

--- Polterheist/Assets/Scripts/Possessables/PossessableRBManager.cs
+++ Polterheist/Assets/Scripts/Possessables/PossessableRBManager.cs
@@ -31,6 +31,8 @@ public class PossessableRBManager : MonoBehaviour
         springJoint.connectedBody = rb;
         springJoint.autoConfigureConnectedAnchor = false;
         springJoint.connectedAnchor = anchorPoint;
+        springJoint.maxDistance = 0;
+        springJoint.spring = 1000;
 
         // TODO: Find appropriate spring strength
         // TODO: Might want to reduce gravity for possessables, so they don't drag as much
