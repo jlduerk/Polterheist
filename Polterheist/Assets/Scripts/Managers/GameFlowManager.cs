575993ee4d1a5b6ae41ff79b886c1c0d852ccd42
DylValevv <dylan.valev@gmail.com>
2023-10-14 15:27:00 -0400

    imported MEC

--- Polterheist/Assets/Scripts/Managers/GameFlowManager.cs
+++ Polterheist/Assets/Scripts/Managers/GameFlowManager.cs
@@ -1,18 +1,38 @@
+using MEC;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameFlowManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GameFlowManager : MonoBehaviour {
+    public int numRounds = 3;
+    public float roundStartDelay = 2;
+    private int roundsCounter = 0;
+    private CoroutineHandle beginRoundCoroutine;
+
+    public void BeginRound() {
+        if (!beginRoundCoroutine.IsRunning) {
+            beginRoundCoroutine = Timing.RunCoroutine(_BeginRound());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private IEnumerator<float> _BeginRound() {
+        //TODO send a message for screen transitions OR wait time prior to game start
         
+        yield return Timing.WaitForSeconds(roundStartDelay);
+        GameManager.Instance.StartGame();
+    } 
+
+    private void NextRound() {
+        roundsCounter++;
+        if (roundsCounter >= numRounds) {
+            GameOver();
+            return;
+        }
+
+        BeginRound();
+    }
+
+    private void GameOver() {
+        //TODO
     }
 }
