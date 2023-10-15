0ee23a41073cf9300cab64ebdb11c971e8f417e6
kslokhandwala2022 <kslokhandwala@wpi.edu>
2023-10-14 21:41:30 -0400

    ghost jitter fix

--- Polterheist/Assets/Scripts/Player/Ghost.cs
+++ Polterheist/Assets/Scripts/Player/Ghost.cs
@@ -11,7 +11,6 @@ public class Ghost : MonoBehaviour
     private const string SHADER_SLANTDIR_REFERENCE = "_SlantDir";
     public float wiggleSlantMultiplier = 0.5f;
     private const string SHADER_WIGGLE_SPEED_REFERENCE = "_WiggleSpeed";
-    public float wiggleSpeedMultiplier = 0.5f;
     
     private static readonly int wiggleSlant = Shader.PropertyToID(SHADER_SLANTDIR_REFERENCE);
     private static readonly int wiggleSpeed = Shader.PropertyToID(SHADER_WIGGLE_SPEED_REFERENCE);
@@ -26,7 +25,14 @@ public class Ghost : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        material.SetFloat(wiggleSpeed, rb.velocity.magnitude * wiggleSpeedMultiplier + 1);
+        if(new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude > .2 )
+        {
+            material.SetFloat(wiggleSpeed, 9);
+        }
+        else
+        {
+            material.SetFloat(wiggleSpeed, 3);
+        }
         material.SetVector(wiggleSlant, rb.velocity * wiggleSlantMultiplier);
     }
 }
