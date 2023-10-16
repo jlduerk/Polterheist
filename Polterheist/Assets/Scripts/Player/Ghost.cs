6f17e15e0f2ae43e3a60bab12afd73f2b33871d2
DylValevv <dylan.valev@gmail.com>
2023-10-15 21:51:56 -0400

    dropshadow!

--- Polterheist/Assets/Scripts/Player/Ghost.cs
+++ Polterheist/Assets/Scripts/Player/Ghost.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ghost : MonoBehaviour
-{
+public class Ghost : MonoBehaviour {
     public Rigidbody rb;    
     public Transform target;
     public Renderer renderer;
@@ -14,25 +13,41 @@ public class Ghost : MonoBehaviour
     
     private static readonly int wiggleSlant = Shader.PropertyToID(SHADER_SLANTDIR_REFERENCE);
     private static readonly int wiggleSpeed = Shader.PropertyToID(SHADER_WIGGLE_SPEED_REFERENCE);
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
+    
+    public LayerMask groundLayer;
+    public ParticleSystem dropShadow;
+    public Vector3 dropShadowOffset = new Vector3(0, 0.25f, 0);
+    public float dropShadowDistanceScalar = 2;
+    private const int MAX_DROPSHADOW_DISTANCE = 100;
+    
+    void Start() {
         material = renderer.material;
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude > .2 )
-        {
+    
+    void Update() {
+        if(new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude > .2 ) {
             material.SetFloat(wiggleSpeed, 9);
         }
-        else
-        {
+        else {
             material.SetFloat(wiggleSpeed, 3);
         }
         material.SetVector(wiggleSlant, rb.velocity * wiggleSlantMultiplier);
+
+        DropShadow();
+    }
+
+    private void DropShadow() {
+        if (!dropShadow) {
+            Debug.LogWarning($"No dropShadow Particle System reference on {name}!", gameObject);
+            return;
+        }
+        
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, MAX_DROPSHADOW_DISTANCE, groundLayer)) {
+            dropShadow.transform.position = hit.point + dropShadowOffset;
+            float distance = transform.position.y - hit.point.y;
+            dropShadow.transform.localScale = Vector3.one * (distance * dropShadowDistanceScalar);
+        }
     }
 }
