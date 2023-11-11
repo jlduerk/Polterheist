e0b1a4b01c7e25681cbd432ac74941aad58bac6b
kslokhandwala2022 <kslokhandwala@wpi.edu>
2023-11-11 18:58:36 -0500

    feat: add hat movement to ghost player
    The hat at this point statically moves around mirroring the ghost wiggle
    speed. What would be even better is to have it rotate slightly with
    the ghost head.
    
    If we want "soft" hats, I could also make another shader that wiggles
    like the ghost, but on top of the ghost head.

--- Polterheist/Assets/Scripts/Player/Ghost.cs
+++ Polterheist/Assets/Scripts/Player/Ghost.cs
@@ -7,13 +7,14 @@ public class Ghost : MonoBehaviour {
     public Transform target;
     public Renderer renderer;
     private Material material;
+    public Renderer hatRenderer;
+    private Material hatMaterial;
     private const string SHADER_SLANTDIR_REFERENCE = "_SlantDir";
     public float wiggleSlantMultiplier = 0.5f;
     private const string SHADER_WIGGLE_SPEED_REFERENCE = "_WiggleSpeed";
 
     private static readonly int wiggleSlant = Shader.PropertyToID(SHADER_SLANTDIR_REFERENCE);
     private static readonly int wiggleSpeed = Shader.PropertyToID(SHADER_WIGGLE_SPEED_REFERENCE);
-    
     public LayerMask groundLayer;
     public ParticleSystem dropShadow;
     public Vector3 dropShadowOffset = new Vector3(0, 0.25f, 0);
@@ -22,14 +23,26 @@ public class Ghost : MonoBehaviour {
     
     void Start() {
         material = renderer.material;
+        if(hatRenderer != null)
+        {
+            hatMaterial = hatRenderer.material;
+        }
     }
-    
+
     void Update() {
         if(new Vector3(rb.velocity.x, 0, rb.velocity.z).magnitude > .2 ) {
             material.SetFloat(wiggleSpeed, 9);
+            if (hatMaterial != null)
+            {
+                hatMaterial.SetFloat(wiggleSpeed, 9);
+            }
         }
         else {
             material.SetFloat(wiggleSpeed, 3);
+            if (hatMaterial != null)
+            {
+                hatMaterial.SetFloat(wiggleSpeed, 3);
+            }
         }
         material.SetVector(wiggleSlant, rb.velocity * wiggleSlantMultiplier);
 
