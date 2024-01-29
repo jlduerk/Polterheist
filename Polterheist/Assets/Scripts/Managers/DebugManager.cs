7bd164bcdc4b7cf8a55b416980755b1a48df5cc0
majohadev <62721323+majohadev@users.noreply.github.com>
2024-01-29 00:17:30 -0500

    Add togglable keyboard control support, via the DebugManager
    Instead of turning on keyboard support by default for players, this adds a way to turn it on manually for individual players.  Hit the F10 key to cycle between all players (a debug log will print the current one) and you will be able to control that player with the keyboard.

--- Polterheist/Assets/Scripts/Managers/DebugManager.cs
+++ Polterheist/Assets/Scripts/Managers/DebugManager.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
 
 [System.Serializable]
 public struct DebugKey {
     public enum FunctionType {
         Reset = 0,
+        CycleKeyboardPlayer,
     }
     
     public KeyCode keyCode;
@@ -60,6 +63,9 @@ public class DebugManager : MonoBehaviour {
             case DebugKey.FunctionType.Reset:
                 ResetGame();
                 break;
+            case DebugKey.FunctionType.CycleKeyboardPlayer:
+                CycleKeyboardPlayer();
+                break;
         }
 
         Debug.Log(EventSystem.current.currentSelectedGameObject);
@@ -69,6 +75,67 @@ public class DebugManager : MonoBehaviour {
     #region Debug Commands
     private void ResetGame() {
         SceneManager.LoadScene(MAIN_MENU_SCENE_NAME);
+    }
+
+    // Index of the player to control with the keyboard; -1 means no players are controlled with keyboard
+    private int controlledPlayer = -1;
+    // Switches keyboard input to the next player
+    private void CycleKeyboardPlayer() {
+        //PlayerInput[] players = FindObjectsByType<PlayerInput>(FindObjectsSortMode.InstanceID);
+        int playerCount = PlayerInput.all.Count;
+        if (playerCount == 0)
+        {
+            controlledPlayer = -1;
+            return;
+        }
+
+        InputDevice keyboardDevice = InputSystem.GetDevice("Keyboard");
+        if (keyboardDevice == null)
+            return;
+
+        if (controlledPlayer >= playerCount)
+            controlledPlayer = -1;
+
+        Func<PlayerInput, InputDevice> FindGamepad = player => 
+        {
+            foreach(InputDevice device in player.devices)
+            {
+                if (device is Gamepad)
+                    return device;
+            }
+            return null;
+        };
+
+        if (controlledPlayer > -1)
+        {
+            PlayerInput player = PlayerInput.GetPlayerByIndex(controlledPlayer);
+            player.user.UnpairDevice(keyboardDevice);
+            InputDevice gamepadDevice = FindGamepad(player);
+            if (gamepadDevice == null)
+                player.SwitchCurrentControlScheme("Gamepad");
+            else
+                player.SwitchCurrentControlScheme("Gamepad", gamepadDevice);
+        }
+
+        controlledPlayer++;
+        if (controlledPlayer >= playerCount)
+            controlledPlayer = -1;
+
+        if (controlledPlayer > -1)
+        {
+            PlayerInput player = PlayerInput.GetPlayerByIndex(controlledPlayer);
+            InputUser.PerformPairingWithDevice(keyboardDevice, user: player.user);
+            InputDevice gamepadDevice = FindGamepad(player);
+            if (gamepadDevice == null)
+                player.SwitchCurrentControlScheme("Keyboard", keyboardDevice);
+            else
+                player.SwitchCurrentControlScheme("Keyboard", keyboardDevice, gamepadDevice);
+            Debug.Log($"DEBUG: Player {controlledPlayer} is now controlled by the keyboard.");
+        }
+        else
+        {
+            Debug.Log($"DEBUG: No players are being controlled by the keyboard.");
+        }
     }
 
     private void ShowCursor() {
